77afa9a6b7b999b637320203a7e9f27157df6d9b
xmmmmmovo <xmmmmmovo@users.noreply.github.com>
2020-06-16 02:47:17 +0000

    deploy: 47791ef9f441e725501b44a78bf455deab5f0b1c

---
+++ precache-manifest.7cc3b62657a7d79bd2641220db63e63a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58154aeafd224410dcf7639d422f828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d3bd61e0b7f00612b56",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "272275292317dfa3c847",
+    "url": "/static/js/2.c1b73918.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c1b73918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3bd61e0b7f00612b56",
+    "url": "/static/js/main.c7b019d2.chunk.js"
+  },
+  {
+    "revision": "078f7bb298154a746164",
+    "url": "/static/js/runtime-main.a1bbc6e5.js"
+  }
+]);
