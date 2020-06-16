391b8fe7031ec0a211edff9742893170a546300b
xmmmmmovo <13256172360@163.com>
2020-06-16 18:19:18 +0800

    upload mysql dao

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './App.css'
+import { Button } from 'antd'
+import 'mqtt'
 
 type Props = {
-
 };
 export const App = (props: Props) => {
   return (
-    <div>
-            appp
+    <div className={'container'}>
+      <Button type={'primary'}>
+          aaa
+      </Button>
     </div>
   )
 }
