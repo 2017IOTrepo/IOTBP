efcbf37d18ec8af5a0123828cf30f0d8d7ab77b3
xmmmmmovo <xmmmmmovo@users.noreply.github.com>
2020-06-16 02:58:54 +0000

    deploy: f99ba2970634a15944f7121e8dc9b24748fb2097

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b389d3160df149ca6d8cf11e4dabf829.js"
+  "/IOTBP/precache-manifest.b69dde61b386e7985f480efd939ee1c6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IOTBP/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
